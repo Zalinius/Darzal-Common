e365f23023286e658b92b926c3d9cc2b31a6ab14
Darzington <nclparmentier@gmail.com>
2024-01-12 11:53:21 -0500

    Added the standard java consumers as null consumers

--- src/main/java/com/darzalgames/darzalcommon/functional/Consumers.java
+++ src/main/java/com/darzalgames/darzalcommon/functional/Consumers.java
@@ -1,6 +1,6 @@
 package com.darzalgames.darzalcommon.functional;
 
-import java.util.function.Consumer;
+import java.util.function.*;
 
 /**
  * A class for convenience consumers
@@ -12,7 +12,28 @@ public class Consumers {
 	 * @param <E> The Consumer's type
 	 * @return A consumer which has no effect when accepting.
 	 */
-	public static <E> Consumer<E> nullConsumer(){
+	public static <E> Consumer<E> nullConsumer() {
+		return e -> {};
+	}
+	
+	/**
+	 * @return An IntConsumer which has no effect when accepting.
+	 */
+	public static IntConsumer nullIntegerConsumer() {
+		return e -> {};
+	}
+	
+	/**
+	 * @return An DoubleConsumer which has no effect when accepting.
+	 */
+	public static DoubleConsumer nullDoubleConsumer() {
+		return e -> {};
+	}
+	
+	/**
+	 * for with new ja@return ASeeing LongConsumer which has no effect when accepting.
+	 */
+	public static LongConsumer nullLongConsumer() {
 		return e -> {};
 	}
 }
