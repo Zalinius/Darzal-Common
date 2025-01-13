0ad1a1c89369bf598e19fd537117637100883566
Darzington <32342957+Darzington@users.noreply.github.com>
2025-01-13 13:13:11 -0500

    Refactor the hexagon screen position math to be simpler (#32)

--- src/main/java/com/darzalgames/darzalcommon/hexagon/HexagonMath.java
+++ src/main/java/com/darzalgames/darzalcommon/hexagon/HexagonMath.java
@@ -6,8 +6,6 @@ public class HexagonMath {
 
 	/**
 	 * Convert the flat-top hexagon's logical coordinates to pixel coordinates
-	 * @param height The height component in the height:width ratio
-	 * @param width The width component in the height:width ratio
 	 * @param q The Q-axis coordinate of the hexagon
 	 * @param r The R-axis coordinate of the hexagon
 	 * @param hexagonWidth The width of the visual representation of the hexagon
@@ -16,16 +14,14 @@ public class HexagonMath {
 	 * @return A {@link Tuple} with the pixel coordinates for where this {@link Hexagon}'s visual representation should be drawn on the Stage 
 	 * 	relative to other hexagon's in the visual representation of the {@link HexagonMap})
 	 */
-	public static Tuple<Float, Float> getScreenPositionOnStage(float height, float width, int q, int r, float hexagonWidth, float hexagonHeight, float stageHeight) {
-		Tuple<Float, Float> position = getScreenPosition(height, width, q, r, hexagonWidth, hexagonHeight);
+	public static Tuple<Float, Float> getScreenPositionOnStage(int q, int r, float hexagonWidth, float hexagonHeight, float stageHeight) {
+		Tuple<Float, Float> position = getScreenPosition(q, r, hexagonWidth, hexagonHeight);
 		float y = stageHeight - position.f - hexagonHeight; // This math works from a top-left system and actors work from bottom-left, so we subtract from the screen height
 		return new Tuple<Float, Float>(position.e, y);
 	}
 
 	/**
 	 * Convert the flat-top hexagon's logical coordinates to pixel coordinates. The hex at coordinates (0, 0) is always located at position (0, 0)
-	 * @param height The height component in the height:width ratio
-	 * @param width The width component in the height:width ratio
 	 * @param q The Q-axis coordinate of the hexagon
 	 * @param r The R-axis coordinate of the hexagon
 	 * @param hexagonWidth The width of the visual representation of the hexagon
@@ -33,10 +29,10 @@ public class HexagonMath {
 	 * @return A {@link Tuple} with the screen position coordinates for where this {@link Hexagon}'s visual representation should be drawn
 	 * 	relative to other hexagon's in the visual representation of the {@link HexagonMap})
 	 */
-	public static Tuple<Float, Float> getScreenPosition(float width, float height, int q, int r, float hexagonWidth, float hexagonHeight) {
+	public static Tuple<Float, Float> getScreenPosition(int q, int r, float hexagonWidth, float hexagonHeight) {
 		float size = hexagonWidth/2f;
 		float x = size * (3f/2 * q);
-		float y = size * ((height/width)*q + ((height*2)/width) * r);
+		float y = size * ((hexagonHeight/hexagonWidth)*q + ((hexagonHeight*2)/hexagonWidth) * r);
 		return new Tuple<Float, Float>(x, y);
 	}
 	
